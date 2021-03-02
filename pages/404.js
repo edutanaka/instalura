7f662881c09b14595a7a25e7242daa4ef5ba0747
edutanaka <edutanakabr@gmail.com>
2021-03-02 14:38:59 -0300

    add 404 and sobre

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Box } from '../src/components/foundation/layout/Box';
+import Menu from '../src/components/commons/Menu';
+import { Text } from '../src/components/foundation/Text';
+import Footer from '../src/components/commons/Footer';
 
 export default function Page404() {
   return (
-    <div>
-      Página 404
-    </div>
+    <>
+      <Box>
+        <Menu />
+        <Text 
+          tag="h1"
+          textAlign={{md: 'center'}}
+        >
+          404
+        </Text>
+        <Footer />
+      </Box>
+
+    </>
   );
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Footer from '../src/components/commons/Footer';
+import Menu from '../src/components/commons/Menu';
+import { Box } from '../src/components/foundation/layout/Box';
+import { Text } from '../src/components/foundation/Text';
 
 export default function PageSobre() {
   return (
-    <div>
-      Página Sobre
-    </div>
+    <>
+      <Box>
+        <Menu />
+        <Text 
+          tag="h1"
+          textAlign={{md: 'center'}}
+        >
+          sobre
+        </Text>
+        <Footer />
+      </Box>
+
+    </>
   );
 }
