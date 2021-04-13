188cdd977fc8a8c4d978286cb761f451d64794ca
edutanaka <edutanakabr@gmail.com>
2021-04-13 17:04:23 -0300

    test(login form): work with schema in login form

--- src/components/patterns/FormLogin/index.js
+++ src/components/patterns/FormLogin/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import * as yup from 'yup';
 import { Button } from '../../commons/Button';
 import TextField from '../../forms/TextField';
 import { useForm } from '../../../infra/hooks/forms/useForm';
 import { loginService } from '../../../services/login/loginService';
+
+const loginSchema = yup.object().shape({
+  usuario: yup
+    .string()
+    .required('usuário é obrigatório')
+    .min(3, 'preencha ao menos 3 caracteres'),
+
+  senha: yup
+    .string()
+    .required('senha é obrigatória')
+    .min(8, 'sua senha precisa ter ao menos 8 caracteres'),
+});
 
 export default function LoginForm() {
   const router = useRouter();
@@ -22,6 +35,11 @@ export default function LoginForm() {
           router.push('/app/profile');
         });
     },
+    async validateSchema(values) {
+      return loginSchema.validate(values, {
+        abortEarly: false,
+      });
+    },
   });
 
   return (
@@ -31,6 +49,9 @@ export default function LoginForm() {
         name="usuario"
         value={form.values.usuario}
         onChange={form.handleChange}
+        error={form.errors.usuario}
+        isTouched={form.touched.usuario}
+        onBlur={form.handleBlur}
       />
       <TextField
         placeholder="Senha"
@@ -38,6 +59,9 @@ export default function LoginForm() {
         type="password"
         value={form.values.senha}
         onChange={form.handleChange}
+        error={form.errors.senha}
+        isTouched={form.touched.senha}
+        onBlur={form.handleBlur}
       />
 
       <Button
@@ -48,9 +72,13 @@ export default function LoginForm() {
           md: 'initial',
         }}
         fullWidth
+        disabled={form.isFormDisabled}
       >
         Entrar
       </Button>
+      <pre>
+        {JSON.stringify(form.touched, null, 4)}
+      </pre>
     </form>
   );
 }
--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-export function useForm({ initialValues, onSubmit }) {
+export function useForm({
+  initialValues,
+  onSubmit,
+  validateSchema,
+}) {
   const [values, setValues] = React.useState(initialValues);
+
+  const [isFormDisabled, setIsFormDisabled] = React.useState(true);
+  const [errors, setErrors] = React.useState({});
+  const [touched, setTouchedFields] = React.useState({});
+
+  React.useEffect(() => {
+    validateSchema(values)
+      .then(() => {
+        setIsFormDisabled(false);
+        setErrors({});
+      })
+      .catch((err) => {
+        const fomartedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
+          const fieldName = currentError.path;
+          const errorMessage = currentError.message;
+
+          return {
+            ...errorObjectAcc,
+            [fieldName]: errorMessage,
+          };
+        }, {});
+
+        setErrors(fomartedErrors);
+        setIsFormDisabled(true);
+      });
+  }, [values]);
 
   return {
     values,
@@ -17,6 +47,17 @@ export function useForm({ initialValues, onSubmit }) {
         ...currentValues,
         [fieldName]: value,
       }));
+    },
+    isFormDisabled,
+    errors,
+    touched,
+    handleBlur(event) {
+      const fieldName = event.target.getAttribute('name');
+
+      setTouchedFields({
+        ...touched,
+        [fieldName]: true,
+      });
     },
   };
 }
