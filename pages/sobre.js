22a8e1d799c7c81950ddc1d5c67aac4f23e4da65
edutanaka <edutanakabr@gmail.com>
2021-04-21 19:57:46 -0300

    feat(cms): update CMS components
    refactor CMS' code delegating between infra and screen components

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,27 +1,8 @@
-import { GraphQLClient, gql } from 'graphql-request';
-import AboutScreen from '../src/components/screens/AboutScreen';
+import AboutScreen, { getContent } from '../src/components/screens/AboutScreen';
 import websitePageHOC from '../src/components/wrappers/WebsitePage/hoc';
 
 export async function getStaticProps() {
-  const TOKEN = '69126cd3c348d9f3732df15837794f';
-  const DatoCMSURL = 'https://graphql.datocms.com/';
-
-  const client = new GraphQLClient(DatoCMSURL, {
-    headers: {
-      Authorization: `Bearer ${TOKEN}`,
-    },
-  });
-
-  const query = gql`
-    query {
-      pageSobre {
-        pageTitle
-        pageDescription
-      }
-    }
-  `;
-
-  const messages = await client.request(query);
+  const messages = await getContent();
 
   return {
     props: {
