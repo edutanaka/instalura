991038b1f20018a02f3deb128bd8b119bb588545
edutanaka <edutanakabr@gmail.com>
2021-04-24 13:17:48 -0300

    feat(authentication): add authentication in login page

---
+++ src/services/user/userService.js
@@ -0,0 +1,25 @@
+import { BASE_URL } from '../../infra/env/isStagingEnv';
+import { HttpClient } from '../../infra/http/HttpClient';
+import { authService } from '../auth/authService';
+
+export const userService = {
+  async getProfilePage(ctx) {
+    const url = `${BASE_URL}/api/users/posts`;
+    try {
+      const token = await authService(ctx).getToken();
+      const response = await HttpClient(url, {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      });
+      return {
+        user: {
+          totalLikes: 100,
+        },
+        posts: response.data,
+      };
+    } catch (err) {
+      throw new Error('Não conseguimos pegar os posts');
+    }
+  },
+};
